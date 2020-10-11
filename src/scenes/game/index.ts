6b37407c58cbaa57d3f3a45caa113fbd22756690
Alexandre Figueiredo <alexfgrdo@gmail.com>
2020-10-11 13:35:14 +0200

    Add cursor keys to the Game scene

--- src/scenes/game/index.ts
+++ src/scenes/game/index.ts
@@ -1,10 +1,16 @@
 import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
+	private keys!: Phaser.Types.Input.Keyboard.CursorKeys;
+
 	constructor() {
 		super('game');
 	}
 
-	preload() {}
+	preload() {
+		//
+		// Création des inputs du clavier
+		this.keys = this.input.keyboard.createCursorKeys();
+	}
 	create() {}
 }
