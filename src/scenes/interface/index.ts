7557acddbf6078ce009597cf1ef84a089b7e34ae
Alexandre Figueiredo <alexfgrdo@gmail.com>
2020-10-11 16:41:17 +0200

    Create Interface scene

---
+++ src/scenes/interface/index.ts
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+import { sceneEvent } from '../../utils';
+
+export default class Interface extends Phaser.Scene {
+	constructor() {
+		super({ key: 'interface' });
+	}
+
+	create() {}
+}
